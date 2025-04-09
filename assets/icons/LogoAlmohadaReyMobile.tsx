db9ffb33a87b294147fb03852fbf8f396688e14d
alxFv <worfysito15@gmail.com>
2025-04-09 10:32:57 -0500

    feat: ajustar efectos de animación y estilos en logotipos, eliminar sombras y escalar en hover

--- assets/icons/LogoAlmohadaReyMobile.tsx
+++ assets/icons/LogoAlmohadaReyMobile.tsx
@@ -19,14 +19,11 @@ export const LogoAlmohadaReyMobile = ({
     height={height}
     viewBox="0 0 504.5 437"
     preserveAspectRatio="xMidYMid meet"
-    style={{
-      display: "block",
-      overflow: "visible",
-    }}
+    style={{ display: "block" }}
     {...props}
   >
     <defs>
-      <style>{`.st0{fill:${color}; stroke:rgba(255,255,255,0.2); stroke-width:0.4px;}`}</style>
+      <style>{`.st0{fill:${color}}`}</style>
     </defs>
     <path
       d="M231 34h35l-28.3 20.6L248.5 88l-28.4-20.6L191.8 88l10.8-33.4L174.2 34h35.1L220.1.7 231 34zm161.9 0H428l-28.4 20.6L410.5 88l-28.4-20.6L353.7 88l10.9-33.4L336.2 34h35.1L382.1.7 392.9 34z"
