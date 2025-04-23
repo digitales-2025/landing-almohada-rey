d7ae0d385cfb47326ab0f7c75c3f367c17109cb6
Gonzalo <gonzalogut.99@gmail.com>
2025-04-23 13:11:12 -0500

    feat: cambiar el tema predeterminado a light y desactivar el sistema de temas

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -59,8 +59,8 @@ export default async function RootLayout({
             >
                 <ThemeProvider
                     attribute="class"
-                    defaultTheme="system"
-                    enableSystem
+                    defaultTheme="light"
+                    // enableSystem
                     disableTransitionOnChange
                 >
                     <NextIntlClientProvider>
