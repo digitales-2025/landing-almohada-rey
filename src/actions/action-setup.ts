acdf1f52814b0c75007ba3517008d22409e454db
DiegoRondonR <furrtr4532@gmail.com>
2025-05-30 15:54:14 -0500

    feat: add baseUri constant from environment configuration in action-setup

--- src/actions/action-setup.ts
+++ src/actions/action-setup.ts
@@ -1,6 +1,9 @@
+import { envs } from '@/config/envs';
 import { BookingOps } from './booking/booking.di';
 import container from './inversify-setup';
 import { RoomOps, RoomTypeOps } from './rooms/room.di';
+
+export const baseUri = envs.BACKEND_URL;
 
 export const bookingOps = container.get(BookingOps);
 export const roomTypeOps = container.get(RoomTypeOps);
